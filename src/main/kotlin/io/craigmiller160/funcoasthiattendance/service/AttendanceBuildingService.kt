2839aeee802160cf584093d6ca8001122efcdfa4
Craig Miller <craigmiller@Craigs-MacBook-Pro-2.local>
2022-11-01 10:43:01 -0400

    Fixing things

--- src/main/kotlin/io/craigmiller160/funcoasthiattendance/service/AttendanceBuildingService.kt
+++ src/main/kotlin/io/craigmiller160/funcoasthiattendance/service/AttendanceBuildingService.kt
@@ -1,6 +1,8 @@
 package io.craigmiller160.funcoasthiattendance.service
 
 import arrow.core.flatMap
+import io.craigmiller160.funcoasthiattendance.model.AttendanceRecord
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -10,11 +12,21 @@ class AttendanceBuildingService(
   private val attendanceParsingService: AttendanceParsingService,
   private val databasePopulatingService: DatabasePopulatingService
 ) {
+  private val log = LoggerFactory.getLogger(javaClass)
   @Transactional
   fun build() {
     resetService
       .resetAllData()
       .flatMap { attendanceParsingService.parse() }
       .flatMap { databasePopulatingService.populate(it) }
+      .fold(this::logException, this::logSuccess)
+  }
+
+  private fun logException(ex: Throwable) {
+    log.error("Error building attendance data", ex)
+  }
+
+  private fun logSuccess(records: List<AttendanceRecord>) {
+    log.info("Successfully built attendance data")
   }
 }
