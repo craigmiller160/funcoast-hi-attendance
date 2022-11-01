88cf5047e7a4d00b7ff72cff1c0065db6681dd7f
Craig Miller <craigmiller@Craigs-MacBook-Pro-2.local>
2022-11-01 10:02:49 -0400

    More csv parsing

--- src/main/kotlin/io/craigmiller160/funcoasthiattendance/service/AttendanceParsingService.kt
+++ src/main/kotlin/io/craigmiller160/funcoasthiattendance/service/AttendanceParsingService.kt
@@ -1,6 +1,7 @@
 package io.craigmiller160.funcoasthiattendance.service
 
 import arrow.core.Either
+import com.opencsv.CSVReader
 import io.craigmiller160.funcoasthiattendance.function.TryEither
 import org.springframework.core.io.ResourceLoader
 import org.springframework.stereotype.Service
@@ -10,6 +11,11 @@ class AttendanceParsingService(private val resourceLoader: ResourceLoader) {
   companion object {
     private const val FILE = "attendance.csv"
   }
-  fun parse(): TryEither<String> =
-    Either.catch { resourceLoader.getResource(FILE).inputStream.reader().readText() }
+  fun parse(): TryEither<Sequence<Array<String>>> =
+    Either.catch {
+      CSVReader(resourceLoader.getResource(FILE).inputStream.reader())
+        .readAll()
+        .asSequence()
+        .drop(1)
+    }
 }
