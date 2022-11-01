758e4a430e0fe0c9f722dc19de1ffd8981f9684f
Craig Miller <craigmiller@Craigs-MacBook-Pro-2.local>
2022-11-01 09:52:15 -0400

    Initial commit

---
+++ src/main/kotlin/io/craigmiller160/funcoasthiattendance/FuncoastHiAttendanceApplication.kt
@@ -0,0 +1,11 @@
+package io.craigmiller160.funcoasthiattendance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FuncoastHiAttendanceApplication
+
+fun main(args: Array<String>) {
+	runApplication<FuncoastHiAttendanceApplication>(*args)
+}
---
+++ src/test/kotlin/io/craigmiller160/funcoasthiattendance/FuncoastHiAttendanceApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.craigmiller160.funcoasthiattendance
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FuncoastHiAttendanceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
