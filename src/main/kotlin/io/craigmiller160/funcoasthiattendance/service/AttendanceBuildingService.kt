65b5692eb73496f3b8b927303cca7701b97f6114
Craig Miller <craigmiller@Craigs-MacBook-Pro-2.local>
2022-11-01 10:06:49 -0400

    Integrated parsing

--- src/main/kotlin/io/craigmiller160/funcoasthiattendance/service/AttendanceBuildingService.kt
+++ src/main/kotlin/io/craigmiller160/funcoasthiattendance/service/AttendanceBuildingService.kt
@@ -3,6 +3,8 @@ package io.craigmiller160.funcoasthiattendance.service
 import org.springframework.stereotype.Service
 
 @Service
-class AttendanceBuildingService {
-  fun build() {}
+class AttendanceBuildingService(private val attendanceParsingService: AttendanceParsingService) {
+  fun build() {
+    attendanceParsingService.parse().map { recs -> recs.forEach { println(it) } }
+  }
 }
