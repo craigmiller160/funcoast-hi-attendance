160072dcad1097a595f4c0a01344c98b9331e1a7
Craig Miller <craigmiller@Craigs-MacBook-Pro-2.local>
2022-11-01 09:57:43 -0400

    Beginning setup

--- src/main/kotlin/io/craigmiller160/funcoasthiattendance/FuncoastHiAttendanceApplication.kt
+++ src/main/kotlin/io/craigmiller160/funcoasthiattendance/FuncoastHiAttendanceApplication.kt
@@ -2,9 +2,19 @@ package io.craigmiller160.funcoasthiattendance
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.Async
+import org.springframework.scheduling.annotation.EnableAsync
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
-class FuncoastHiAttendanceApplication
+@EnableAsync
+class FuncoastHiAttendanceApplication {
+	@Async
+	@PostConstruct
+	fun run() {
+
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<FuncoastHiAttendanceApplication>(*args)
---
+++ src/main/kotlin/io/craigmiller160/funcoasthiattendance/model/AttendanceStatus.kt
@@ -0,0 +1,7 @@
+package io.craigmiller160.funcoasthiattendance.model
+
+enum class AttendanceStatus {
+    NEW,
+    MEMBER,
+    RETURNING
+}
