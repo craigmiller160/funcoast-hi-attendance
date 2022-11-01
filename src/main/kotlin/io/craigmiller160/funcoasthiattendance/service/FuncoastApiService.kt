4e52362c3cb5dd36e9a05060c19080bfe4595236
Craig Miller <craigmiller@Craigs-MacBook-Pro-2.local>
2022-11-01 12:09:20 -0400

    Finished preparing for first test

--- src/main/kotlin/io/craigmiller160/funcoasthiattendance/service/FuncoastApiService.kt
+++ src/main/kotlin/io/craigmiller160/funcoasthiattendance/service/FuncoastApiService.kt
@@ -1,5 +1,6 @@
 package io.craigmiller160.funcoasthiattendance.service
 
+import arrow.core.Either
 import io.craigmiller160.funcoasthiattendance.function.TryEither
 import io.craigmiller160.funcoasthiattendance.model.AuthenticationResponse
 import io.craigmiller160.funcoasthiattendance.model.OAuth2Values
@@ -10,20 +11,24 @@ import org.springframework.web.reactive.function.client.WebClient
 
 @Service
 class FuncoastApiService(private val webClient: WebClient, private val oAuth2Values: OAuth2Values) {
-  fun calculateRoster(): TryEither<Unit> = TODO()
+  fun calculateRoster(): TryEither<Unit> {
+    return authenticate().map { Unit }
+  }
 
   private fun authenticate(): TryEither<AuthenticationResponse> {
-    webClient
-      .post()
-      .uri("/oauth2/oauth/token")
-      .header("Authorization", createAuthenticateBasicAuthHeader())
-      .body(
-        BodyInserters.fromFormData("grant_type", "password")
-          .with("username", oAuth2Values.userName)
-          .with("password", oAuth2Values.password))
-      .retrieve()
-      .bodyToMono(AuthenticationResponse::class.java)
-      .block()
+    val result =
+      webClient
+        .post()
+        .uri("/oauth2/oauth/token")
+        .header("Authorization", createAuthenticateBasicAuthHeader())
+        .body(
+          BodyInserters.fromFormData("grant_type", "password")
+            .with("username", oAuth2Values.userName)
+            .with("password", oAuth2Values.password))
+        .retrieve()
+        .bodyToMono(AuthenticationResponse::class.java)
+        .block()!!
+    return Either.Right(result) // TODO clean this up a lot
   }
 
   private fun createAuthenticateBasicAuthHeader(): String {
