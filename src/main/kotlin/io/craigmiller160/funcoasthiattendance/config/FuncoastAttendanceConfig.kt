f722db452da508029bf2ed7ef509079edc561fd1
Craig Miller <craigmiller@Craigs-MacBook-Pro-2.local>
2022-11-01 13:25:41 -0400

    Maybe got automatic date parsing working

--- src/main/kotlin/io/craigmiller160/funcoasthiattendance/config/FuncoastAttendanceConfig.kt
+++ src/main/kotlin/io/craigmiller160/funcoasthiattendance/config/FuncoastAttendanceConfig.kt
@@ -1,8 +1,12 @@
 package io.craigmiller160.funcoasthiattendance.config
 
+import java.time.LocalDate
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
+import org.springframework.format.annotation.DateTimeFormat
 
 @ConstructorBinding
 @ConfigurationProperties(prefix = "funcoast.attendance")
-class FuncoastAttendanceConfig(private val firstMeetingDate: String)
+class FuncoastAttendanceConfig(
+  @DateTimeFormat(pattern = "yyyy-MM-dd") val firstMeetingDate: LocalDate
+)
